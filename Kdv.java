27fe462edc262a05f538248e337f68158a64d408
esmayilmaz <esmanurylmz426@gmail.com>
2025-07-26 14:42:14 +0300

    kdv nin ismi degisti

--- Kdv.java
+++ Kdv.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Kdv {  //  Kullanıcıdan alınan fiyatın kdvli tutarını ve kdv miktarını ekrana bastıran program
     // Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan program
     public static void main(String[] args) {
-        double tutar,kdvOrani,kdv,kdvliFiyat;
+        double tutar,kdvOrani,Kdv,kdvliFiyat;
 
         Scanner input=new Scanner(System.in);
         System.out.print("tutarı giriniz ");
@@ -13,10 +13,10 @@ public class Kdv {  //  Kullanıcıdan alınan fiyatın kdvli tutarını ve kdv 
 
 
         kdvOrani= (tutar <1000) ? 0.18 : 0.08;
-        kdv=tutar*kdvOrani;
-        kdvliFiyat=tutar+kdv;
+        Kdv=tutar*kdvOrani;
+        kdvliFiyat=tutar+Kdv;
 
-        System.out.println("kdv tutarı "+ kdv);
+        System.out.println("kdv tutarı "+ Kdv);
         System.out.println("kdv'li fiyat "+ kdvliFiyat );
 
     }
